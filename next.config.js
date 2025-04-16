dc4f286f5bcc565e35af7021fbb44d8fdfc230ca
Gabriel Eccel <152515435+geccel23@users.noreply.github.com>
2025-04-16 12:44:42 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  ,
-  images: {
-    domains: ['localhost'],
-  },
+  reactStrictMode: true
 };
 
-module.exports = nextConfig;
+export default nextConfig;
