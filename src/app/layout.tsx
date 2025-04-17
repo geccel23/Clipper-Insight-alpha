70f60971562f5b1bec49f79f8924e2d3ef175860
Gabriel Eccel <152515435+geccel23@users.noreply.github.com>
2025-04-16 22:58:33 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import './styles/globals.css';
+import '@/styles/globals.css';
 import { ThemeProvider } from '@/components/ui/theme-provider';
 import { AppDataProvider } from '@/components/AppDataContext';
 
