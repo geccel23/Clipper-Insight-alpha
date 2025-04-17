04b9d2bf0f299a5ce8b4b06a26bc302d3b0ac85f
Gabriel Eccel <152515435+geccel23@users.noreply.github.com>
2025-04-16 23:25:19 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,22 @@
 import '@/styles/globals.css';
 import { ThemeProvider } from '@/components/ui/theme-provider';
 import { AppDataProvider } from '@/components/AppDataContext';
+import { ToastProvider } from '@/components/ui/toaster';
+import { AuthProvider } from '@/components/AuthContext';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <AppDataProvider>{children}</AppDataProvider>
-        </ThemeProvider>
+        <AuthProvider>
+          <ToastProvider>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+              <AppDataProvider>{children}</AppDataProvider>
+            </ThemeProvider>
+          </ToastProvider>
+        </AuthProvider>
       </body>
     </html>
   );
 }
+
